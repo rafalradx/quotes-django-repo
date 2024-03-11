de8a1a2559be9c879c57b46fd4094baa097fe8b7
Rafal Pietras <rafal.radx@gmail.com>
2024-03-12 00:56:19 +0100

    add tag route

--- quotes/quotesapp/views.py
+++ quotes/quotesapp/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from quotesapp.forms import TagForm
+
 
 # Create your views here.
+def main(request):
+    return render(request, "quotesapp/index.html")
+
+
+def tag(request):
+    if request.method == "POST":
+        form = TagForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(to="quotesapp:main")
+        else:
+            return render(request, "quotesapp/tag.html", {"form": form})
+
+    return render(request, "quotesapp/tag.html", {"form": TagForm()})
