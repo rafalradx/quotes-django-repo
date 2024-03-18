a21d0082e3ad32cb49ff3a4c73f1a59604eb6dc1
Rafal Pietras <rafal.radx@gmail.com>
2024-03-18 09:38:32 +0100

    tags display, a search

--- tags/urls.py
+++ tags/urls.py
@@ -6,4 +6,5 @@ app_name = "tags"
 urlpatterns = [
     path("add/", views.add, name="add"),
     path("", views.tags, name="tags"),
+    path("details/<int:tag_id>", views.quotes_with_tag, name="details"),
 ]
--- tags/views.py
+++ tags/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
+from quotesapp.models import Quote, Tag
 from tags.forms import TagForm
 
 
 # Create your views here.
 def tags(request):
-    return HttpResponse("list of tags")
+    tags = Tag.objects.all()
+    return render(request, "tags/display.html", {"tags": tags})
 
 
 def add(request):
@@ -19,3 +21,17 @@ def add(request):
             return render(request, "tags/tag.html", {"form": form})
 
     return render(request, "tags/tag.html", {"form": TagForm()})
+
+
+def quotes_with_tag(request, tag_id):
+    try:
+        tag = Tag.objects.get(id=tag_id)
+    except Tag.DoesNotExist:
+        return HttpResponseNotFound("Tag does not exist")
+    quotes = Quote.objects.filter(tags__id=tag_id)
+    extracted_tags = [quote.tags.all() for quote in quotes]
+    return render(
+        request,
+        "tags/details.html",
+        {"quotes": zip(quotes, extracted_tags), "sel_tag": tag},
+    )
