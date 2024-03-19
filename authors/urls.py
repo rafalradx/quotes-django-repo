53075dd664246d7c613ee7cd4a93b84970a04beb
Rafal Pietras <rafal.radx@gmail.com>
2024-03-20 00:38:28 +0100

    authors from  quotes.toscrape.com add to db

--- authors/urls.py
+++ authors/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("add/", views.add, name="add"),
     path("", views.authors, name="authors"),
     path("details/<int:author_id>", views.details, name="details"),
+    path("scrape/", views.scrape, name="scrape"),
 ]
--- authors/views.py
+++ authors/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import get_object_or_404, render, redirect
 from quotesapp.models import Author, Quote
 from authors.forms import AuthorForm
 from django.core.paginator import Paginator
+from scrap.scrapquotedata import scrap_author_details
+from django.http import HttpResponse
 
 
 # Create your views here.
 def authors(request):
     # authors = Author.objects.all()
-    paginator = Paginator(Author.objects.all(), 10)
+    paginator = Paginator(Author.objects.all(), 8)
     page = request.GET.get("page")
     authors = paginator.get_page(page)
     return render(request, "authors/display.html", {"authors": authors})
@@ -31,3 +33,16 @@ def add(request):
             return render(request, "authors/author.html", {"form": form})
 
     return render(request, "authors/author.html", {"form": AuthorForm()})
+
+
+def scrape(request):
+    author_details = scrap_author_details()
+    for author_scraped in author_details:
+        new_author = Author(
+            fullname=author_scraped.fullname,
+            born_date=author_scraped.born_date,
+            born_location=author_scraped.born_location,
+            description=author_scraped.description,
+        )
+        new_author.save()
+    return redirect(to="authors:authors")
